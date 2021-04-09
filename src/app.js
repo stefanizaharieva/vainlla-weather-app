d9b3ebdeb88ccca0149b3a112c8a41dfa33c16d8
stefanizaharieva <78364258+stefanizaharieva@users.noreply.github.com>
2021-04-09 16:12:08 +0100

    added the search engine

--- src/app.js
+++ src/app.js
@@ -39,8 +39,19 @@ function displayTemperature(response) {
     iconElement.setAttribute("alt", response.data.weather[0].description); 
 }
 
-let apiKey = "4c09a5cae9d6b252db4b54c2b9ab1b65";
-let city = "New York";
-let apiUrl =`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+function search(city) {
+    let apiKey = "4c09a5cae9d6b252db4b54c2b9ab1b65";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+}
 
-axios.get(apiUrl).then(displayTemperature);
+function handleSubmit(event) {
+    event.preventDefault();
+    let cityInputElement = document.querySelector("#city-input");
+    search(cityInputElement.value); 
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", handleSubmit);
+
+search("London"); 
