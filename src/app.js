0fa832ce996efcae0781d0347d0153ea1c011c51
stefanizaharieva <78364258+stefanizaharieva@users.noreply.github.com>
2021-04-15 17:22:26 +0100

    removed the fahrenheit conversion

--- src/app.js
+++ src/app.js
@@ -113,32 +113,4 @@ function handleSubmit(event) {
 
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
-
-function displayFahrenheitTempterature(event) {
-    event.preventDefault(); 
-    let temperatureElement = document.querySelector("#temperature");
-
-    celsiusLink.classList.remove("active");
-    fahrenheitLink.classList.add("active"); 
-    let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32; 
-    temperatureElement.innerHTML = Math.round(fahrenheitTemperature); 
-}
-
-function displayCelsiusTempterature(event) {
-    event.preventDefault();
-    celsiusLink.classList.add("active");
-    fahrenheitLink.classList.remove("active");
-    let temperatureElement = document.querySelector("#temperature");
-    temperatureElement.innerHTML = Math.round(celsiusTemperature); 
-}
-
-let celsiusTemperature = null;  
-
-let fahrenheitLink = document.querySelector("#fahrenheit-link"); 
-fahrenheitLink.addEventListener("click", displayFahrenheitTempterature); 
-
-let celsiusLink = document.querySelector("#celsius-link"); 
-celsiusLink.addEventListener("click", displayCelsiusTempterature); 
-
-
 search("London");
